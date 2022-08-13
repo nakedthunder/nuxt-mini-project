fb74d4d4b90912e46330db46309356871766cd5d
Jess <nakedthunder00@gmail.com>
2022-08-13 23:49:10 +0900

    fixed mainPage

--- components/main/mainPage.vue
+++ components/main/mainPage.vue
@@ -1,7 +1,27 @@
 <template>
     <div class="main_page_img_outter">
-        <div class="img_box" v-for="(img, idx) in mainPageImg" :key="idx">
-            <img :src="img.url" :key="img.id"/>
+        <!-- 장소 카테고리 영역 -->
+        <div class="places_cate_wrap">
+            <div class="list" v-for="(spot , i) in placesList" :key="i">
+                <div class="text">
+                    {{ spot.name }}
+                </div>
+            </div>
+        </div>
+        <!-- 카테고리별 사진 영역  -->
+        <div class="img_wrap">
+            <!-- <div class="listCont" v-for="(item, i) in plcesList" :key="i"> -->
+            <div class="cate_ls">
+                <ul class="ImgList">
+                    <li class="ImgList_ls" v-for="(img, idx) in mainPageImg" :key="idx">
+                        <img :src="img.url" :key="img.id" />
+                    </li>
+                </ul>
+            </div>
+            <!-- <div class="btn">
+                    <span>'{{item.name}}'</span>
+                </div>
+            </div> -->
         </div>
     </div>
 </template>
@@ -10,6 +30,13 @@
 export default {
 data() {
     return {
+        placesList: [
+            { id: 0, name: 'Melbourne' },
+            { id: 1, name: 'Tokyo' },
+            { id: 2, name: 'Taiwan' },
+            { id: 3, name: 'Italy' },
+            { id: 4, name: 'HongKong' },
+        ],
         mainPageImg: [
             { id: 0, url: require('../../assets/image/books.JPG') },
             { id: 1, url: require('../../assets/image/cake.JPG')},
@@ -25,8 +52,16 @@ data() {
 </script>
 
 <style>
-.img_box img {
-    width: 340px;
-    height: 200px;
+.ImgList_ls img {
+    width: 30%;
+}
+
+.main_page_img_outter {
+    text-align: center;
+}
+
+.places_cate_wrap .list {
+    display: inline-block;
+    padding: 10px 20px;
 }
 </style>
